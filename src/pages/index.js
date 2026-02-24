e0ff0dadf8782086793cb19d5f4f6d8264e509bb
rhizal24 <rhizalrhoma@gmail.com>
2026-02-24 10:38:59 +0700

    feat: added github action for case 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter } from '@next/font/google'
-// import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
 
         <div>
-          <h5 className={inter.className}>Rhizal | 514719</h5>
+          <h5 className={inter.className}>[Name] | [NIU]</h5>
         </div>
       </main>
     </>
